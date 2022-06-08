c1a4a705849e2e519c1beccea698bf92c9ccdb98
Ohhyung Kwon <kosuel@gmail.com>
2022-06-09 09:53:54 +1000

    initial commit

---
+++ Pods/Target Support Files/Pods-apod-apodUITests/Pods-apod-apodUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_apod_apodUITests : NSObject
+@end
+@implementation PodsDummy_Pods_apod_apodUITests
+@end
---
+++ apod/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  apod
+//
+//  Created by ohhyung kwon on 9/6/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
