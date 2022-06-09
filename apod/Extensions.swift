246a2e1c74153c06b8028ec2956bae4fa0039a31
Ohhyung Kwon <kosuel@gmail.com>
2022-06-09 16:49:05 +1000

    implemeted data fetch from network and update ui

--- apod/Extensions.swift
+++ apod/Extensions.swift
@@ -12,6 +12,35 @@ enum ApiKeys{
     static let nasaApiKey = "LggVWOT1UkH4vIQkIne5dHB1jHqmhVcTh32EqpzQ"
 }
 
+extension UIColor {
+    static var cellColors: [UIColor] {
+        [
+            UIColor(rgb: 0x1a237e),
+            UIColor(rgb: 0x283593),
+            UIColor(rgb: 0x303f9f),
+            UIColor(rgb: 0x3949ab),
+            UIColor(rgb: 0x3f51b5),
+            UIColor(rgb: 0x5c6bc0)
+        ]
+    }
+    
+    convenience init(red: Int, green: Int, blue: Int) {
+        assert(red >= 0 && red <= 255, "Invalid red component")
+        assert(green >= 0 && green <= 255, "Invalid green component")
+        assert(blue >= 0 && blue <= 255, "Invalid blue component")
+
+        self.init(red: CGFloat(red) / 255.0, green: CGFloat(green) / 255.0, blue: CGFloat(blue) / 255.0, alpha: 1.0)
+    }
+
+    convenience init(rgb: Int) {
+        self.init(
+            red: (rgb >> 16) & 0xFF,
+            green: (rgb >> 8) & 0xFF,
+            blue: rgb & 0xFF
+        )
+    }
+}
+
 extension Date{
     func addDays(_ days:Int) -> Date{        
         return Calendar.current.date(byAdding: .day, value: days, to: self)!
--- apod/ListViewController.swift
+++ apod/ListViewController.swift
@@ -10,11 +10,40 @@ import UIKit
 class ListViewController: UIViewController {
     @IBOutlet weak var tableView: UITableView!
     @IBOutlet weak var nextButton: UIButton!
+    @IBOutlet weak var activityIndicator: UIActivityIndicatorView! // show when fetching...
+    
+    var isFetching = true {
+        didSet{
+            
+            activityIndicator.isHidden = !isFetching
+            tableView.isHidden = isFetching
+        }
+    }
+    
+    var apods = [Apod]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         nextButton.fill(background: .white, text: .label)
+        
+        activityIndicator.isHidden = true
+
+        // start fetch data
+        let today = Date().start.addDays(-1)
+        let oneWeekBefore = today.addDays(-8)
+
+        isFetching = true
+        Apod.fetchApods(from: oneWeekBefore, to: today) { [weak self] apods in
+            
+            guard let self = self else { return}
+            
+            self.apods = apods ?? []
+            
+            self.tableView.reloadData()
+            
+            self.isFetching = false
+        }
     }
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
@@ -32,12 +61,50 @@ class ListViewController: UIViewController {
 
 // MARK: - Tableview data source and delegate
 extension ListViewController: UITableViewDelegate, UITableViewDataSource{
+    
+    enum CellIdentifier: String{
+        case basicCell = "Cell"
+    }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        apods.count
+    }
+    
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        0
+        1
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        UITableViewCell()
+        let cell = tableView.dequeueReusableCell(withIdentifier: CellIdentifier.basicCell.rawValue, for: indexPath)
+        let dateLabel = cell.contentView.viewWithTag(100) as? UILabel
+        let titleLabel = cell.contentView.viewWithTag(101) as? UILabel
+        let image = cell.contentView.viewWithTag(102)
+        
+        let apod = apods[indexPath.section]
+        
+        dateLabel?.text = DateFormatter.localizedString(from: apod.date, dateStyle: .medium, timeStyle: .none)
+        titleLabel?.text = apod.title
+        image?.alpha = apod.isSelected ? 1.0 : 0.0
+        
+        let colors = UIColor.cellColors
+        let colorIndex = indexPath.section % colors.count
+        
+        cell.backgroundColor = colors[colorIndex]
+        
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        apods[indexPath.section].isSelected = !apods[indexPath.section].isSelected
+
+        tableView.reloadRows(at: [indexPath], with: .automatic)
+    }
+    
+    func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        5
+    }
+    func tableView(_ tableView: UITableView, heightForFooterInSection section: Int) -> CGFloat {
+        0
     }
 }
 
